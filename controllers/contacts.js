72ce167f67df0cd977532176e86c131597af0765
a.pinchuk <co.born2code@gmail.com>
2023-04-18 18:11:01 +0200

    finished middlewares

---
+++ controllers/contacts.js
@@ -0,0 +1,52 @@
+const contacts = require('../models/contacts');
+const { HttpError } = require('../helpers/HttpError');
+const { ctrlWrapper } = require('../helpers/ctrlWrapper');
+
+const getAll = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, 'Not Found');
+  }
+  res.json(result);
+};
+
+const add = async (req, res) => {
+  const { name, email, phone } = req.body;
+  const result = await contacts.addContact(name, email, phone);
+  res.status(201).json(result);
+};
+
+const deleteContact = async (req, res) => {
+  const { contactId } = req.params;
+
+  const result = await contacts.removeContact(contactId);
+
+  if (!result) {
+    throw HttpError(404, 'Not found');
+  }
+  res.status(200).json({ message: 'contact deleted' });
+};
+
+const update = async (req, res) => {
+  const { contactId } = req.params;
+
+  const result = await contacts.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, 'Not Found');
+  }
+  res.status(200).json(result);
+};
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getContactById: ctrlWrapper(getContactById),
+  add: ctrlWrapper(add),
+  deleteContact: ctrlWrapper(deleteContact),
+  update: ctrlWrapper(update),
+};
