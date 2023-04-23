2568fa7770d3a5d599085de0e68c225527c398ca
a.pinchuk <co.born2code@gmail.com>
2023-04-23 19:26:22 +0200

    adding mongoose

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require('mongoose');
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  'mongodb+srv://apinchuk:QsTKCaveXuHFVdOy@cluster0.7f87jul.mongodb.net/db_contacts?retryWrites=true&w=majority';
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+    console.log('Database connection successful');
+  })
+  .catch(er => {
+    console.log(er.message);
+    process.exit(1);
+  });
